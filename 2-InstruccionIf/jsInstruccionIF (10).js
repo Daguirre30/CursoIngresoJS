b2d014cd6e85c16fb6a1fa9cf16e672341ff8a22
Daguirre30 <danilugo86@gmail.com>
2017-02-10 15:45:20 -0300

    terminando la instruccion if

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -2,5 +2,27 @@ function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
 	
+	var numeroRandon;
+	
+
+		numeroRandon = Math.floor((Math.random() * 10) + 1);
+		
+		if(numeroRandon == 10 || numeroRandon == 9)
+		{
+			alert("EXCELENTE"); 
+		}
+		else
+		{
+			if(numeroRandon >=4)
+			{
+				alert("APROBÓ");
+			}
+			else
+			{
+				alert("Vamos, la proxima se puede" );
+			}
+		}
+		console.log(numeroRandon);
+	
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -2,9 +2,11 @@ function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
 
-	var numeroRandonmeroRandon = Math.floor((Math.random() * 10) + 1);
+	var numeroRandon;
 
+		numeroRandon = Math.floor((Math.random() * 10) + 1);
 
+	alert(numeroRandon);
 	
 
 }//FIN DE LA FUNCIÓN
