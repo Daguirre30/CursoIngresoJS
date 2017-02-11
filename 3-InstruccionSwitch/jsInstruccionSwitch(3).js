ed09b9eefd8919c5190ae448260eb2ea3bcec481
Daguirre30 <danilugo86@gmail.com>
2017-02-10 22:07:39 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -1,11 +1,32 @@
 function Mostrar()
 {
-//tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+	//tomo la edad  
+	var mesDelAño;
+	mesDelAño = document.getElementById('mes').value;
 
-//alert (mesDelAño);
+	switch (mesDelAño)
+	{
+		case "Febrero":
+		alert(" Este mes no tiene más de 29 días." );
+		break;
+		case "Enero":
+		case "Marzo":
+		case "Abril":
+		case "Mayo":
+		case "Junio":
+		case "Julio":
+		case "Agosto":
+		case "Septiembre":
+		case "Octubre":
+		case "Noviembre":
+		case "Diciembre":
+		alert("Este mes tiene 30 o más días");
+
+	}
+
+		
 	
-	
 
 
-}//FIN DE LA FUNCIÓN
+}
+//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -1,9 +1,30 @@
 function Mostrar()
 {
-//tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+	//tomo la edad  
+	var mesDelAño;
+	mesDelAño = document.getElementById('mes').value;
 
-//alert (mesDelAño);
+	switch(mesDelAño)
+	{
+		case "Febrero":
+		alert(" Este mes no tiene más de 28 días." );
+		break;
+		case "Enero":
+		case "Marzo":
+		case "Mayo":
+		case "Julio":
+		case "Agosto":
+		case "Octubre":
+		case "Diciembre":
+		alert(" este mes tiene 31 días.");
+		break;
+		default:
+		alert(" este mes tiene 30 días.");
+
+
+	}
+
+
 	
 	
 
